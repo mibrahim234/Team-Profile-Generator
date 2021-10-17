bf064e5dfd940679008571b32101f69847285694
mibrahim234 <mohamm1417@gmail.com>
2021-10-17 19:52:14 -0400

    initial index commit

--- main/index.js
+++ main/index.js
@@ -5,3 +5,13 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template");
+
+const teamMembers = [];
+const idArray = [];
+
+
--- main/src/page-template.js
+++ main/src/page-template.js
@@ -26,7 +26,7 @@ const generateTeam = team => {
         <div class="card employee-card"
         <div class = "card-header">
             <h2 class = "card-title">${engineer.getName()}</h2>
-            <h3> class = "card-title"><i class="fas fa mug hot mr-2"></i>${engineer.getRole()}</h3>
+            <h3> class = "card-title"><i class="fas fa-glasses mr-2"</i>${engineer.getRole()}</h3>
         </div>
         <div class = "card-body">
             <ul class="list-group">
@@ -45,7 +45,7 @@ const generateTeam = team => {
         <div class="card employee-card"
         <div class = "card-header">
             <h2 class = "card-title">${intern.getName()}</h2>
-            <h3> class = "card-title"><i class="fas fa mug hot mr-2"></i>${intern.getRole()}</h3>
+            <h3> class = "card-title"><i class="fas fa-user-graduate mr-2></i>${intern.getRole()}</h3>
         </div>
         <div class = "card-body">
             <ul class="list-group">
@@ -90,10 +90,13 @@ module.exports = team => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
+    <link rel="stylesheet" 
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        <link rel="stylesheet" href="../style.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Saira:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
 
 <body>
     <div class="container-fluid">
