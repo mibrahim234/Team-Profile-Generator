3954eb39f7cfbb9f9c778cc99f3bed10b04ad3e1
mibrahim234 <mohamm1417@gmail.com>
2021-10-17 00:54:32 -0400

    engineer and employee test demo

---
+++ main/__test__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can set Github account via constructor", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Sang woo", 1, "test@test.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return Engineer", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Sang woo", 1, "test@test.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get github username via github", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Sang woo", 1, "test@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
