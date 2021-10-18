06dd9c00d57a94bd8d954392be29450b079c5989
mibrahim234 <mohamm1417@gmail.com>
2021-10-17 20:10:37 -0400

    added questions for manager

--- main/index.js
+++ main/index.js
@@ -14,4 +14,58 @@ const render = require("./src/page-template");
 const teamMembers = [];
 const idArray = [];
 
-
+function appMenu() {
+    function promptManager() {
+        console.log("Welcome! Please Build Your Team");
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "managerName",
+                    message: "What is the team manager's name?",
+                    validate: answer => {
+                        if(answer !== '') {
+                            return true;
+                        }
+                        return 'Please enter atleast one character.';
+                    }
+                },
+                {
+                    type: "input",
+                    name: "managerId",
+                    message: "What is the team manager's id?",
+                    validate: answer => {
+                       const pass = answer.match(/^[1-9]\d*$/);
+                        if(pass) {
+                            return true;
+                        }
+                        return 'Please enter a number greater than zero.';
+                    }
+                },
+                {
+                    type: "input",
+                    name: "managerEmail",
+                    message: "What is the team manager's email?",
+                    validate: answer => {
+                        const pass = answer.match
+                        (/\S+@\S+\.\S+/);
+                        if(pass) {
+                            return true;
+                        }
+                        return 'Please enter a valid email address.';
+                    }
+                },
+                {
+                    type: "input",
+                    name: "managerOfficeNumber",
+                    message: "What is the team manager's office number?",
+                    validate: answer => {
+                       const pass = answer.match(/^[1-9]\d*$/);
+                        if(pass) {
+                            return true;
+                        }
+                        return 'Please enter a number greater than zero.';
+                    }
+                }
+            ])
+        }
